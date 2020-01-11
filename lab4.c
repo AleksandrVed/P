cd4efb32e2051ae1aad030f40caa74a48084902e
AleksandrVed <55152650+AleksandrVed@users.noreply.github.com>
2020-01-12 01:12:25 +0300

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -3,34 +3,40 @@
 
 int main(void)
 {
-float a, x, g, f, y, step, x1, x2, H, mini, maxi;
-int var, exitt, n;
+float a, x, g, f, y, step, x1, H, mini, maxi;
+int var, exitt, n, steps;
 float spisok[100];
 exitt = 0;
 while (exitt == 0)
 {
     n = 0;
-    printf ("Vvedite a: \n");
+    printf ("Vvedite na4al'noe a: ");
     scanf ("%f" , &a);
-    printf ("Vvedite nomer: \n");
+    printf ("Vvedite nomer: ");
     scanf ("%int", &var);
-    printf ("nizhnaya granica  ");
-    scanf ("%f", &x1);
-    printf("verhnaya granica  ");
-    scanf ("%f", &x2);
-    printf ("Vvedite x: ");
+    printf ("Vvedite na4al'noe x:  ");
     scanf ("%f", &x);
-    printf ("Vvedite shag  ");
-    scanf ("%f", &step);
+    printf ("Vvedite kone4noe x:  ");
+    scanf ("%f", &x1);
+    printf ("Vvedite koli4estvo shagov  ");
+    scanf ("%i", &steps);
+    if (x>x1)
+    {
+        step = (-(x-x1)/steps);
+    }
+    else
+    {
+        step = ((x1-x)/steps);
+    }
     if (var== 1)
     {
             if (3 * (a*a) - 7 * a * x + 4 * x * x == 0)
             {
-                printf("Nevernoe zna4enie");
+                printf("Nevernoe zna4enie\n");
             }
             else
             {
-                while ((x1 <= x) && (x <= x2))
+                while (x != x1)
                     {
                         g = -((7) * (20 *(a*a) + 11 * a * x - ( 45 * (x*x)))) / (3 *(a*a) - 7 * a * x + 4*x*x);
                         printf("g=%f\n",g);
@@ -53,7 +59,7 @@ while (exitt == 0)
             H = 60 * (a*a) + 88* (a*x) +21 * (x*x);
             if (H!=1 && H!=-1)
             {
-                while ((x1 <= x) && (x <= x2))
+                while (x != x1)
                 {      
                 f = (tan(60 * (a *a) + 88 * (a * x) + 21 * (x*x)));
                 printf("f=%f\n",f);
@@ -72,14 +78,14 @@ while (exitt == 0)
             }
             else
             {
-                printf("Nevernoe zna4enie");
+                printf("Nevernoe zna4enie\n");
             }
         }
     if (var == 3)
     {
             if ((-40) * (a*a) + 3* a* x+ (x*x) +1 > 0)
             {
-                while ((x1 <= x) && (x <= x2))
+                while (x != x1)
                 {
                 y = log((-40) * (a*a) + 3* a* x+ (x*x) +1)/log(2);
                 printf("y=%f\n",y);
@@ -98,14 +104,14 @@ while (exitt == 0)
             }
             else
             {
-                printf("Nevernoe zna4enie");
+                printf("Nevernoe zna4enie\n");
             }
         }
     printf("max = %f ", maxi);
     printf("min = %f\n ", mini);
     if (var > 3 || var < 1)
     {
-        printf("Nevernoe zna4enie");
+        printf("Nevernoe zna4enie\n");
     }
     printf("Prodol)|(ut' raboty? (0-Yes, 1-Net)");
     scanf("%int", &exitt);
