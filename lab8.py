700aa013908adcd4b958b016fcfe79be09bcfe6d
AleksandrVed <55152650+AleksandrVed@users.noreply.github.com>
2020-01-13 17:48:23 +0300

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -2,18 +2,23 @@ import math
 import random
 import time
 begin = time.process_time()
+t = 0
 c = 0
 n = int(input('Количество точек = '))
 r = float(input('Радиус = '))
+T = int(input('Измерение времении каждые...'))
 ax = random.uniform(-5, 5)
 ay = random.uniform(-5, 5)
 for i in range(0, n):
+    f =  open('time.txt', 'a')
     x = random.uniform(-5, 5)
     y = random.uniform(-5, 5)
     if (math.sqrt((x-ax)*(x-ax))+math.sqrt((y-ay)*(y-ay))<=r):
         c += 1
+    t = t+ 1
+    if t == T:
+        time1 = (time.process_time() - begin)
+        f.write(str(time1)+'  секунд\n')
+        t = 0
 print('Точек в области: ', c)
-time = (time.process_time()-begin)
-f =  open('time.txt', 'a')
-f.write(str(time)+'  секунд')
 f.close
